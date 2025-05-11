65dc87c6944de8f00102a1e6f955c647f6c9ecc6
jaime López <al232901@edu.uaa.mx>
2025-05-11 01:16:25 -0600

    bootstrap agregado

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from "../hero/hero.component";
+import { CarruselComponent } from '../homeComponents/carrusel/carrusel.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent],
+  imports: [HeroComponent,CarruselComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
