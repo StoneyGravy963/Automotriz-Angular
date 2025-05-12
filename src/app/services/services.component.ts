2049c4ddd276f7cc47bcacc7ef917867f4d0eb47
jaime López <al232901@edu.uaa.mx>
2025-05-12 15:17:56 -0600

    Tabla de services agregada

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormularioComponent } from "../formulario/formulario.component";
 import { FormularioReactivoComponent } from "../formulario-reactivo/formulario-reactivo.component";
+import { TablaComponent } from '../servicesComponents/tabla/tabla.component';
 
 @Component({
   selector: 'app-services',
-  imports: [FormularioComponent,FormularioReactivoComponent],
+  imports: [FormularioComponent,FormularioReactivoComponent,TablaComponent],
   templateUrl: './services.component.html',
   styleUrl: './services.component.css'
 })
