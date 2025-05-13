e8d6526f9ea0cabaffcff99102d4998668429db0
jaime López <al232901@edu.uaa.mx>
2025-05-12 20:04:11 -0600

    Services terminado

---
+++ src/app/servicesComponents/mecanicos/mecanicos.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-mecanicos',
+  imports: [
+    MatCardModule
+  ],
+  templateUrl: './mecanicos.component.html',
+  styleUrl: './mecanicos.component.css'
+})
+export class MecanicosComponent {
+  mecanicos = [
+  {
+    nombre: 'Adal Yahir de Luna Nieves ID:335944',
+    especialidad: 'Especialista en frenos y suspensión',
+    foto: '../yahir.jpg'
+  },
+  {
+    nombre: 'Elías López Gutiérrez ID:210356',
+    especialidad: 'Diagnóstico computarizado y electrónica',
+    foto: '../elias.jpg'
+  },
+  {
+    nombre: 'Jaime Iván López Gutiérrez ID:232901',
+    especialidad: 'Cambio de aceite y afinaciones',
+    foto: '../jaime.jpg'
+  }
+];
+
+}
